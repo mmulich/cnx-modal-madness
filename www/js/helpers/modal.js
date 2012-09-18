5d8d7694210499c8d945634a32074e60f5faab49
Michael Mulich <michael.mulich@gmail.com>
2012-09-18 16:00:54 -0400

    Build the modal in the modal class so that we have the opportunity to move its form actions and attach a handler to them.

--- www/js/helpers/modal.js
+++ www/js/helpers/modal.js
@@ -1,5 +1,5 @@
-define(['jquery', 'app/views', 'app/datasource', 'btstrp/bootstrap-modal'],
-       function($, views, datasource) {
+define(['jquery', 'app/templates', 'app/views', 'app/datasource', 'btstrp/bootstrap-modal'],
+       function($, templates, views, datasource) {
 
     function Modal(id, opener, view, container) {
         /* This assumes that the bootstrap data api is enabled and
@@ -7,15 +7,28 @@ define(['jquery', 'app/views', 'app/datasource', 'btstrp/bootstrap-modal'],
          * action.
          */
         this.id = id;
+        this.render = function() {
+            var html = views[view](datasource.get());
+            $html = $(html);
+            if ($html.filter('.form-actions')) {
+                // Put the actions in the footer of the modal.
+                $
+            }
+            this.$modal.filter('.modal-body').html($html);
+        }
+
+
+        // -- init --
         if (container === undefined) {
             container = 'body';
         }
-        // XXX Replace with a mustache template.
-        var $modal = $('<div id="' + id + '" class="modal hide fade in"></div>').appendTo(container);
-        this.render = function() {
-            var html = views.metadataModal(datasource.get());
-            $modal.html(html);
-        }
+        this.$modal = $(templates.modalBase({id: id,
+                                             title: 'Edit metaata',
+                                             body: '<h1>Hello World</h1>',
+                                             actions: '<h3>Action buttons</h3>',
+                                            })).appendTo(container);
+
+
         $(opener).modal({show:false});
         $modal.on('show', $.proxy(this.render));
         return this;
